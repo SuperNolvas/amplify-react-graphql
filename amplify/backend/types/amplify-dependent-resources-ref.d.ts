9f98fae970314892ff5a6377455bacf970d73529
Phil Evans <pde_repair@hotmail.com>
2024-02-04 22:50:04 +0000

    Revert "Update Project pages and add placeholder for upcoming projects."
    This reverts commit d5fabc4789d086ba48409d4525c641e0b555460f.

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,26 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "amplifyreactgraphql": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
+  "auth": {
+    "amplifyreactgraphql1459fe2c": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
+  "storage": {
+    "s32a3a2dc8": {
+      "BucketName": "string",
+      "Region": "string"
+    }
+  }
+}
